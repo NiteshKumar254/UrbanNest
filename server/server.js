c4baa1e18bd5b0ff803fb32065326a0a24dac4db
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-08-07 15:55:28 +0530

    working with backend

--- server/server.js
+++ server/server.js
@@ -4,10 +4,16 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 
 
 connectDB();
+connectCloudinary()
 
 const app=express()
 app.use(cors()) //Enable Cross-Origin Resource Sharing 
@@ -21,6 +27,12 @@ app.use("/api/clerk", clerkWebhooks)
 
 
 app.get('/', (req,res)=>res.send("API is Working Fineee"))
+app.use('/api/user', userRouter); // User routes)
+app.use('/api/hotels', hotelRouter); // Hotel routes);
+app.use('/api/rooms', roomRouter );
+app.use('/api/bookings', bookingRouter ); // Booking routes);
+
+
 
 const PORT=process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server Running on part ${PORT}`) );
